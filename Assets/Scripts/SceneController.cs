6c54d7b1aa6d912d8a0166277618ccc64f11bab4
Erick Simões <erick.simoes@live.com>
2019-09-25 15:30:38 -0300

    When click on Library button, call the Library scene

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+    public void CallScene(string sceneName) {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
