7f51ae6e17d2a48b53b9b2619224441715224bb1
Erick Simões <erick.simoes@live.com>
2019-09-25 19:15:43 -0300

    Add basic system of scene management

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour {
+    public Image loadImage;
     public void CallScene(string sceneName) {
-        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        loadImage.gameObject.SetActive(true);
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
     }
+
 }
