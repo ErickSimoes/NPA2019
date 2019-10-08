85d16cedb17a958ec94bb841bc5efdcc0f086efb
rabih-dev <tabatchnikrabih@gmail.com>
2019-10-08 17:26:08 -0300

    Taking Several Screenshots

--- Assets/Scripts/Screenshot.cs
+++ Assets/Scripts/Screenshot.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Screenshot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int screenshotsTaken;
     void Start()
     {
-        
+        if(!PlayerPrefs.HasKey("Screenshots"))
+        {
+            PlayerPrefs.SetInt("Screenshots", 0);
+        }
+
+        screenshotsTaken = PlayerPrefs.GetInt("Screenshots");
     }
 
     // Update is called once per frame
     public void TakeScreenshot()
     {
-        ScreenCapture.CaptureScreenshot("miiiiiitico.png");
+        screenshotsTaken = screenshotsTaken = PlayerPrefs.GetInt("Screenshots");
+        ScreenCapture.CaptureScreenshot("NPA2019_"+ screenshotsTaken + ".png");
+        PlayerPrefs.SetInt("Screenshots", screenshotsTaken + 1);
     }
+
+    
 }
